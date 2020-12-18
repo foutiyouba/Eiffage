035ee99e8a98ded7c7d8c17265623bf1168f9fb1
foutiyou <foutiyouba@groupeidyal.com>
2020-12-18 21:54:02 +0000

    commit

---
+++ src/main/java/eiffage/EiffageApplication.java
@@ -0,0 +1,13 @@
+package eiffage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiffageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiffageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eiffage/EiffageApplicationTests.java
@@ -0,0 +1,13 @@
+package eiffage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiffageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
