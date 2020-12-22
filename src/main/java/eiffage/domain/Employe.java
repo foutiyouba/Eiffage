d246444f3b6723ad95fe138c86f95c1ca0109fa1
foutiyou <foutiyouba@groupeidyal.com>
2020-12-22 20:58:13 +0000

    commit

--- src/main/java/eiffage/domain/Employe.java
+++ src/main/java/eiffage/domain/Employe.java
@@ -1,4 +1,15 @@
 package eiffage.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Employe")
 public class Employe {
+
+    @Column(name = "id")
+    @Id// SANS ID DAKOY BAGN
+    private Integer id;
+    @Column(name = "code")
+    private String code;
 }
